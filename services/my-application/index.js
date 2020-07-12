1e808400a9ff816171577922e96abe6313776212
1Mill <erik.ekberg@outlook.com>
2020-07-12 11:31:26 -0700

    Run express server

--- services/my-application/index.js
+++ services/my-application/index.js
@@ -1,1 +1,9 @@
-console.log("hello world!");
+const express = require('express');
+const server = express();
+
+server.get('/', (_request, response) => {
+	response.send('Hello world!');
+});
+server.listen(process.env.PORT, () => {
+	console.log(`Listening on port ${process.env.PORT}`);
+});
