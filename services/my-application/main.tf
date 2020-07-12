855cb153979acf0f2137e50ef79e9f7a095e9a3e
1Mill <erik.ekberg@outlook.com>
2020-07-12 16:19:22 -0700

    Update main.tf

--- services/my-application/main.tf
+++ services/my-application/main.tf
@@ -1,3 +1,12 @@
+provider "aws" {
+	// Environment AWS_ACCESS_KEY_ID
+	// Environment AWS_PROFILE
+	// Environment AWS_SECRET_ACCESS_KEY
+
+	region="us-east-1"
+	version = "~> 2.59"
+}
+
 module "production" {
 	source = "./terraform/application"
 	environment = [
