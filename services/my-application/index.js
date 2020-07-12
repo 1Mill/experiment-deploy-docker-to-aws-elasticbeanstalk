3cf9d7ee4977e7b834c8fc919f3059254af2fce1
1Mill <erik.ekberg@outlook.com>
2020-07-12 12:53:33 -0700

    Update index.js

--- services/my-application/index.js
+++ services/my-application/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (_request, response) => {
-	response.send('Hello world!');
+	response.send('Hello world! Hello world!');
 });
 server.listen(process.env.PORT, () => {
 	console.log(`Listening on port ${process.env.PORT}`);
