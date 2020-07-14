d999bcbf653f55bd3a0b57872a66d874448fcb21
1Mill <erik.ekberg@outlook.com>
2020-07-14 13:55:58 -0700

    Move contents of state.tf into main.tf

--- services/my-application/main.tf
+++ services/my-application/main.tf
@@ -1,3 +1,21 @@
+terraform {
+	required_version = "~> 0.12.28"
+
+	backend "s3" {
+		// access_key = ENVIRONMENT AWS_ACCESS_KEY_ID
+		// profile = ENVIRONMENT AWS_PROFILE
+		// region = ENVIRONMENT AWS_DEFAULT_REGION
+		// secret_key = ENVIRONMENT AWS_SECRET_ACCESS_KEY
+
+		bucket = "experiment-terraform-state"
+		dynamodb_table = "experiment-terraform-state-locks"
+		encrypt = true
+		key = "global/s3/terraform.tfstate"
+		profile = "terraform"
+		region = "us-east-1"
+	}
+}
+
 provider "aws" {
 	// Environment AWS_ACCESS_KEY_ID
 	// Environment AWS_PROFILE
