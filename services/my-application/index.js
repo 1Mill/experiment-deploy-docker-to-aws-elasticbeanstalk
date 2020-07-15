36bdf936a9b5232859b7b9b56147fb08658be453
1Mill <erik.ekberg@outlook.com>
2020-07-14 20:02:32 -0700

    Update index.js

--- services/my-application/index.js
+++ services/my-application/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (_request, response) => {
-	response.send('Release to production!');
+	response.send('CI/CDelivery is a real thing!');
 });
 server.listen(process.env.PORT, () => {
 	console.log(`Listening on port ${process.env.PORT}`);
