9a40d223803e72edc2ae584e12c1de31f0dc1716
1Mill <erik.ekberg@outlook.com>
2020-07-18 20:50:11 -0700

    Try persion released images to dockerhub

--- services/my-second-application/index.js
+++ services/my-second-application/index.js
@@ -4,7 +4,7 @@ const server = express();
 server.get('/', (_request, response) => {
 	response.send(`
 		<h1>Hello world from my-second-application!!</h1>
-		klsdjflksdf
+		klsdjflksdfsfsdf
 	`);
 });
 server.listen(process.env.PORT, () => {
