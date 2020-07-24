4b566b98a5c2cb3f67248ab5a242f585b146a186
1Mill <erik.ekberg@outlook.com>
2020-07-23 21:09:45 -0700

    Update main.tf

--- packages/terraform/aws/msk/main.tf
+++ packages/terraform/aws/msk/main.tf
@@ -1,30 +1,47 @@
-resource "aws_msk_cluster" {
-	cluster_name = "delete-this-msk-cluster"
-	kafka_version = var.kafka_version
-	number_of_broker_nodes = 3
+data "aws_availability_zones" "default" {
+	state = "available"
+}
 
-	broker_node_group_info  {
-		client_subnets = ""
-		ebs_volume_size = "100"
-		instance_type = "kafka.t3.small"
-		security_groups = ""
-	}
+resource "aws_vpc" "default" {
+	cidr_block = "192.168.0.0/22"
 }
-resource "aws_msk_configuration" "default" {
-	kafka_versions = [var.kafka_version]
-	name = var.name
-	server_properties = <<PROPERTIES
-		# AWS Defaults
-		default.replication.factor=3
-		min.insync.replicas=2
-		num.io.threads=8
-		num.network.threads=5
-		num.partitions=1
-		num.replica.fetchers=2
-		socket.request.max.bytes=104857600
-		unclean.leader.election.enable=true
-		# Custom defaults
-		auto.create.topics.enable = true
-		delete.topic.enable = true
-	PROPERTIES
+resource "aws_subnet" "default" {
+	count = length(data.aws_availability_zones.default.names)
+
+	availability_zone = data.aws_availability_zones.default.names[count.index]
+	cidr_block = "192.168.${count.index}.0/24"
+	vpc_id = aws_vpc.default.id
 }
+resource "aws_security_group" "default" {
+	vpc_id = aws_vpc.default.id
+}
+// resource "aws_msk_cluster" {
+// 	cluster_name = "delete-this-msk-cluster"
+// 	kafka_version = var.kafka_version
+// 	number_of_broker_nodes = 3
+
+// 	broker_node_group_info  {
+// 		client_subnets = ""
+// 		ebs_volume_size = "100"
+// 		instance_type = "kafka.t3.small"
+// 		security_groups = ""
+// 	}
+// }
+// resource "aws_msk_configuration" "default" {
+// 	kafka_versions = [var.kafka_version]
+// 	name = var.name
+// 	server_properties = <<PROPERTIES
+// 		# AWS Defaults
+// 		default.replication.factor=3
+// 		min.insync.replicas=2
+// 		num.io.threads=8
+// 		num.network.threads=5
+// 		num.partitions=1
+// 		num.replica.fetchers=2
+// 		socket.request.max.bytes=104857600
+// 		unclean.leader.election.enable=true
+// 		# Custom defaults
+// 		auto.create.topics.enable = true
+// 		delete.topic.enable = true
+// 	PROPERTIES
+// }
