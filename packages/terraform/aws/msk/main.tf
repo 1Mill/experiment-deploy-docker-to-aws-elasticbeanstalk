9582d5988a0e5cbff80af904d69b53596236f145
1Mill <erik.ekberg@outlook.com>
2020-07-24 14:30:41 -0700

    Update main.tf

--- packages/terraform/aws/msk/main.tf
+++ packages/terraform/aws/msk/main.tf
@@ -9,7 +9,7 @@ resource "aws_subnet" "default" {
 	count = length(data.aws_availability_zones.default.names)
 
 	availability_zone = data.aws_availability_zones.default.names[count.index]
-	cidr_block = cidrsubnet(aws_vpc.default.cidr_block, 24, count.index)
+	cidr_block = cidrsubnet(aws_vpc.default.cidr_block, 16, count.index)
 	vpc_id = aws_vpc.default.id
 }
 // resource "aws_security_group" "default" {
