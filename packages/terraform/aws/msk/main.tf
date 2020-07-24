67c63da7a402ef2c205e430ae9edc1c642202804
1Mill <erik.ekberg@outlook.com>
2020-07-23 21:32:26 -0700

    Update main.tf

--- packages/terraform/aws/msk/main.tf
+++ packages/terraform/aws/msk/main.tf
@@ -5,28 +5,28 @@ data "aws_availability_zones" "default" {
 resource "aws_vpc" "default" {
 	cidr_block = "192.168.0.0/22"
 }
-resource "aws_subnet" "default" {
-	count = length(data.aws_availability_zones.default.names)
+// resource "aws_subnet" "default" {
+// 	count = length(data.aws_availability_zones.default.names)
 
-	availability_zone = data.aws_availability_zones.default.names[count.index]
-	cidr_block = cidrsubnet(aws_vpc.default.cidr_block, 8, count.index)
-	vpc_id = aws_vpc.default.id
-}
-resource "aws_security_group" "default" {
-	vpc_id = aws_vpc.default.id
-}
-resource "aws_msk_cluster" "default" {
-	cluster_name = "delete-this-msk-cluster"
-	kafka_version = var.kafka_version
-	number_of_broker_nodes = 1 * length(data.aws_availability_zones.default.names)
+// 	availability_zone = data.aws_availability_zones.default.names[count.index]
+// 	cidr_block = cidrsubnet(aws_vpc.default.cidr_block, 8, count.index)
+// 	vpc_id = aws_vpc.default.id
+// }
+// resource "aws_security_group" "default" {
+// 	vpc_id = aws_vpc.default.id
+// }
+// resource "aws_msk_cluster" "default" {
+// 	cluster_name = "delete-this-msk-cluster"
+// 	kafka_version = var.kafka_version
+// 	number_of_broker_nodes = 1 * length(data.aws_availability_zones.default.names)
 
-	broker_node_group_info  {
-		client_subnets = aws_subnet.default[*].id
-		ebs_volume_size = "100"
-		instance_type = "kafka.t3.small"
-		security_groups = [aws_security_group.default.id]
-	}
-}
+// 	broker_node_group_info  {
+// 		client_subnets = aws_subnet.default[*].id
+// 		ebs_volume_size = "100"
+// 		instance_type = "kafka.t3.small"
+// 		security_groups = [aws_security_group.default.id]
+// 	}
+// }
 // resource "aws_msk_configuration" "default" {
 // 	kafka_versions = [var.kafka_version]
 // 	name = var.name
