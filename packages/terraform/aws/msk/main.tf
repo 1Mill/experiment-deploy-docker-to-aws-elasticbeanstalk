637bc95bed110988923f9f67bb3fb35a23a98e6c
1Mill <erik.ekberg@outlook.com>
2020-07-25 16:32:20 -0700

    Update main.tf

--- packages/terraform/aws/msk/main.tf
+++ packages/terraform/aws/msk/main.tf
@@ -1,6 +1,10 @@
 data "aws_availability_zones" "default" {
 	state = "available"
 }
+data "aws_msk_configuration" "default" {
+	// Manually managed
+	name = "2-4-1--${var.zone_count}-replication-factor"
+}
 
 resource "aws_vpc" "default" {
 	cidr_block = "192.168.0.0/16"
@@ -27,26 +31,7 @@ resource "aws_msk_cluster" "default" {
 		security_groups = [aws_security_group.default.id]
 	}
 	configuration_info {
-		arn = aws_msk_configuration.default.arn
-		revision = aws_msk_configuration.default.latest_revision
+		arn = data.aws_msk_configuration.default.arn
+		revision = data.aws_msk_configuration.default.latest_revision
 	}
-}
-resource "aws_msk_configuration" "default" {
-	// ! There is not API to delete this resource once created
-	kafka_versions = [var.kafka_version]
-	name = var.name
-	server_properties = <<PROPERTIES
-		# AWS Defaults
-		default.replication.factor=${var.zone_count}
-		min.insync.replicas=2
-		num.io.threads=8
-		num.network.threads=5
-		num.partitions=1
-		num.replica.fetchers=2
-		socket.request.max.bytes=104857600
-		unclean.leader.election.enable=true
-		# Custom defaults
-		auto.create.topics.enable = true
-		delete.topic.enable = true
-	PROPERTIES
 }
