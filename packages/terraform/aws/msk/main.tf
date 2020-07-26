47627335f9a028206764b96ac11c01d9d1680148
1Mill <erik.ekberg@outlook.com>
2020-07-26 10:46:05 -0700

    More ordering of resources

--- packages/terraform/aws/msk/main.tf
+++ packages/terraform/aws/msk/main.tf
@@ -6,16 +6,6 @@ data "aws_msk_configuration" "default" {
 	name = "2-4-1--${var.zone_count}-replication-factor"
 }
 
-resource "aws_vpc" "default" {
-	cidr_block = "192.168.0.0/16"
-}
-resource "aws_subnet" "default" {
-	count = length(slice(data.aws_availability_zones.default.names, 0, var.zone_count))
-
-	availability_zone = data.aws_availability_zones.default.names[count.index]
-	cidr_block = cidrsubnet(aws_vpc.default.cidr_block, 8, count.index)
-	vpc_id = aws_vpc.default.id
-}
 resource "aws_msk_cluster" "default" {
 	cluster_name = var.name
 	kafka_version = var.kafka_version
@@ -34,4 +24,14 @@ resource "aws_msk_cluster" "default" {
 }
 resource "aws_security_group" "default" {
 	vpc_id = aws_vpc.default.id
+}
+resource "aws_subnet" "default" {
+	count = length(slice(data.aws_availability_zones.default.names, 0, var.zone_count))
+
+	availability_zone = data.aws_availability_zones.default.names[count.index]
+	cidr_block = cidrsubnet(aws_vpc.default.cidr_block, 8, count.index)
+	vpc_id = aws_vpc.default.id
+}
+resource "aws_vpc" "default" {
+	cidr_block = "192.168.0.0/16"
 }
